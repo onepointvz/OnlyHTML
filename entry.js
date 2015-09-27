28691a4c90cb4b98ceb61ff4dd937822efc5110c
onepointvz <vzwallet@gmail.com>
2015-09-27 08:07:53 -0500

    Create entry.js

---
+++ entry.js
@@ -0,0 +1,28 @@
+Ext.application({
+	name: 'wallet',
+	appFolder: 'http://onepoint-vzw.rhcloud.com/res/scripts/app',
+	requires: ['wallet.view.LoginView', 'wallet.view.DecisionView', 'wallet.view.CashView', 'wallet.view.AddPayeeView'],
+	controllers: ['VZWalletController'],
+	launch: function() {
+		Ext.create('Ext.container.Viewport',{
+			renderTo: Ext.getBody(),
+			id: 'viewport',
+			itemId: 'viewport',
+			layout: 'fit',
+			items:[{
+				xtype: 'loginview',
+				itemId: 'loginview'
+			},
+			{
+				xtype: 'decisionview',
+				hidden: true
+			},{
+				xtype: 'cashview',
+				hidden: true
+			},{
+				xtype: 'addpayeeview',
+				hidden: true
+			}]
+		});
+	}
+});
